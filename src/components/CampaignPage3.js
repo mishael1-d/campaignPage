119870f2e56dbd2285eaa809177e14bfbce65d74
Mishael Dada <Mishaeldada@gmail.com>
2022-04-12 15:26:05 +0100

    Fixed multiple-image selection

--- src/components/CampaignPage3.js
+++ src/components/CampaignPage3.js
@@ -1,49 +1,47 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import addBtn from "../assets/plus.png";
 import { AppContext } from "../App";
-import Buttons from "./Buttons"
+import Buttons from "./Buttons";
 function CampaignPage3() {
-  const { appState, setAppState,setEnable } = useContext(AppContext);
+  const { appState, setAppState, setEnable } = useContext(AppContext);
+  const [selectedImages, setSelectedImages] = useState([]);
 
-  
   const onFileChange = (e) => {
-    const reader = new FileReader();
-    reader.onload = () => {
-      if (reader.readyState === 2) {
-        const { ...newImage } = appState;
-        newImage.campaignImage.push(reader.result);
-        if (newImage.campaignImage.length > 0) {
-          setEnable(true);
-        } else {
-          setEnable(false);
-        }
-        setAppState(newImage);
-      }
-    };
-    reader.readAsDataURL(e.target.files[0]);
+    const { ...newImage } = appState;
+    if (e.target.files) {
+      const fileArray = Array.from(e.target.files).map((file) =>
+        URL.createObjectURL(file)
+      );
+newImage.campaignImage = fileArray
+      setSelectedImages((prevImages) => prevImages.concat(fileArray));
+    }
+    setAppState(newImage)
+    if (newImage.campaignImage.length > 0) {
+      setEnable(true)
+    }
+
+  };
+  const renderPhotos = (source) => {
+    return source.map((photo) => {
+      return <img src={photo} alt="" key={photo} />;
+    });
   };
   return (
     <>
-    <div className="imagePage">
-      <h3 className="heading">Please Select Image</h3>
-      <div className="imageContainer">
-        <div className="addImage addBtn">
-          <input type="file" multiple id="image" onChange={onFileChange} />
-          <label htmlFor="image">
-            <img src={addBtn} alt="" />
-          </label>
+      <div className="imagePage">
+        <h3 className="heading">Please Select Image</h3>
+        <div className="imageContainer">
+          <div className="addImage addBtn">
+            <input type="file" multiple id="image" onChange={onFileChange} />
+            <label htmlFor="image">
+              <img src={addBtn} alt="" />
+            </label>
+          </div>
+          <div className="addImage image">{renderPhotos(selectedImages)}</div>
         </div>
-        <div className="addImage image">
-        {appState.campaignImage.map((src) => {
-          return (
-              <img src={src} alt="" key={src}/>
-              );
-            })}
-            </div>
       </div>
-    </div>
       <Buttons />
-      </>
+    </>
   );
 }
 
